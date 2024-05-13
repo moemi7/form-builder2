48f2673b482327f8cede3c773e1bad038e50f0cf
Mohamed Taouil <moemi7@hotmail.com>
2024-05-13 19:53:59 +0200

    delete files

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,14 @@ const nextConfig = {
 
    // pageExtensions: ['js', 'jsx', 'mdx'],
 
-   // pages: {
+    pages: {
       // Replace 'customPagesFolder' with the name of your custom folder
       // You can add multiple folders if needed
-    //  'appPages': {
+    'appPages': {
         // Optionally, you can specify any additional configuration for this folder
         // For example, if you want to use a different layout for pages in this folder
         // layout: 'special-layout'
-    //  }}
+      }}
   }
    
   export default nextConfig
