79f30e1dea3a178d8b4a491427eca4319e1972d2
Mohamed Taouil <moemi7@hotmail.com>
2024-05-13 19:23:58 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
    // output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './dist', // Changes the build output directory to `./dist/`.
+
+    pageExtensions: ['js', 'jsx', 'mdx'],
+
+    pages: {
+      // Replace 'customPagesFolder' with the name of your custom folder
+      // You can add multiple folders if needed
+      'appPages': {
+        // Optionally, you can specify any additional configuration for this folder
+        // For example, if you want to use a different layout for pages in this folder
+        // layout: 'special-layout'
+      }}
   }
    
   export default nextConfig
